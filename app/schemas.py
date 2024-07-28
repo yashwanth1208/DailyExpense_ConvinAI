60319015979d4b155a48dd38a3fe5c6ac1ead342
Yashwanth Sai Sollu <www.sai1929@gmail.com>
2024-07-28 15:51:24 +0530

    modified schema to autoschema

--- app/schemas.py
+++ app/schemas.py
@@ -1,22 +1,10 @@
-from marshmallow import Schema, fields, validate
+from . import ma
+from .models import User, Expense
 
-class UserSchema(Schema):
-    id = fields.Int(dump_only=True)
-    email = fields.Email(required=True)
-    name = fields.Str(required=True)
-    mobile_number = fields.Str(required=True)
+class UserSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = User
 
-class ExpenseSchema(Schema):
-    id = fields.Int(dump_only=True)
-    description = fields.Str(required=True)
-    amount = fields.Float(required=True)
-    split_method = fields.Str(required=True, validate=validate.OneOf(['equal', 'exact', 'percentage']))
-    user_id = fields.Int(required=True)
-    participants = fields.Nested('ParticipantSchema', many=True)
-
-class ParticipantSchema(Schema):
-    id = fields.Int(dump_only=True)
-    user_id = fields.Int(required=True)
-    expense_id = fields.Int(required=True)
-    amount = fields.Float()
-    percentage = fields.Float()
+class ExpenseSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Expense
